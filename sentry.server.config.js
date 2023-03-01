1bd58b551fe77282cf90f25ab8d37e8e3bce47c5
dzubaidi <85535667+dzubaidi@users.noreply.github.com>
2023-03-02 02:49:27 +0700

    update sentry properties

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -9,20 +9,10 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   environment: "production",
   dsn: SENTRY_DSN || 'https://42884a9d4ef3406e818d4bdc87b8830e@o1091269.ingest.sentry.io/4504764589473792',
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
-  integrations: [
-    new Sentry.Replay({
-      // Additional SDK configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
 });
